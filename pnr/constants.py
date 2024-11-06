af0cd94853b816fbef99331a7d67d700b3a99f53
Mohit Prajapat <mohit.prajapat@trootech.com>
2024-11-06 19:18:15 +0530

    Update Constants

---
+++ pnr/constants.py
@@ -0,0 +1,24 @@
+# PNR Scrapping Constants
+
+
+class ModelVerbose:
+    """Model Verbose Names"""
+
+    TRAIN_NUMBER = "train number"
+    TRAIN_NAME = "train name"
+    BOARDING_DATE = "boarding date"
+    RESERVED_FROM = "reserved from"
+    RESERVED_TO = "reserved to"
+    RESERVED_CLASS = "reserved class"
+    FARE = "fare"
+    REMARK = "remark"
+    TRAIN_STATUS = "train status"
+    NAME = "name"
+    BOOKING_STATUS = "booking status"
+    CURRENT_STATUS = "current status"
+
+
+class ModelsConstants:
+    """Models Constants"""
+
+    PNR_DETAILS = "pnr_details"
--- pnr/models.py
+++ pnr/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+# Pnr Scrapping Details Model
+from django.db import models
+from pnr.constants import ModelsConstants, ModelVerbose
+
+
+class PnrDetails:
+    """Store PNR Details"""
+
+    train_number = models.IntegerField(verbose_name=ModelVerbose.TRAIN_NUMBER)
+    train_name = models.CharField(max_length=132, verbose_name=ModelVerbose.TRAIN_NAME)
+    boarding_date = models.DateField(verbose_name=ModelVerbose.BOARDING_DATE)
+    reserved_from = models.CharField(verbose_name=ModelVerbose.RESERVED_FROM)
+    reserved_to = models.CharField(verbose_name=ModelVerbose.RESERVED_TO)
+    reserved_class = models.CharField(verbose_name=ModelVerbose.RESERVED_CLASS)
+    fare = models.DecimalField(
+        decimal_places=2, max_digits=10, verbose_name=ModelVerbose.FARE
+    )
+    remark = models.CharField(verbose_name=ModelVerbose.REMARK)
+    train_status = models.CharField(verbose_name=ModelVerbose.TRAIN_STATUS)
+
+
+class PassengerDetails:
+    """Store Passenger Details"""
+
+    pnr_details = models.ForeignKey(
+        "pnr.PnrDetails",
+        on_delete=models.CASCADE,
+        related_name=ModelsConstants.PNR_DETAILS,
+    )
+    name = models.CharField(verbose_name=ModelVerbose.NAME)
+    booking_status = models.CharField(verbose_name=ModelVerbose.BOOKING_STATUS)
+    current_status = models.CharField(verbose_name=ModelVerbose.CURRENT_STATUS)
